159f3beb74d23268ce33e0222dfaecccfffc3a36
IgorMHesse <igorhesse24032009@gmail.com>
2026-02-25 19:22:06 -0300

    Delete rpg-game directory

--- rpg-game/src/rpg_game/guerreiro.py
+++
@@ -1,7 +0,0 @@
-from personagem import Personagem
-
-class Guerreiro(Personagem):
-
-    def usar_escudo(self):
-        print(f"🛡️ {self.nome} usou o escudo e recuperou 10 de vida!")
-        self.vida += 10
